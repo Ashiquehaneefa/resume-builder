553f94d29ceef822933fd45f35afd4758c31f56c
Ashiquehaneefa <93711110+Ashiquehaneefa@users.noreply.github.com>
2022-03-13 17:38:21 +0530

    v1

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const loginRouter = require("./src/routes/LoginRoute");
 const signupRouter = require("./src/routes/SignupRoute");
 const emailvalidator = require("email-validator");
 
+const port = process.env.PORT || 5000
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"))
+}
+
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -104,6 +110,6 @@ app.post("/resumedetails", function (req, res) {
 });
 
 // Port number
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Listening on port 5000");
 });
